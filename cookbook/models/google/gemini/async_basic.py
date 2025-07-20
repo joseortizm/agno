c2415433d173ce0019a345df347577e17872d209
Jose <pepeisa.ortiz@gmail.com>
2025-07-20 17:05:08 -0500

    check gemini

--- cookbook/models/google/gemini/async_basic.py
+++ cookbook/models/google/gemini/async_basic.py
@@ -5,15 +5,15 @@ from agno.models.google import Gemini
 
 agent = Agent(
     model=Gemini(
-        id="gemini-2.0-flash-001",
-        instructions=["You are a basic agent that writes short stories."],
+        id="gemini-2.0-flash",
+        instructions=["Tu eres un docente especializado en ayudar a los estudiantes con discapacidad a desarrollar habilidades digitales"],
     ),
     markdown=True,
 )
 
 # Get the response in a variable
-# run: RunResponse = agent.run("Share a 2 sentence horror story")
-# print(run.content)
+run: RunResponse = agent.run("Que recomendación puedes dar a un estudiante que tiene dificultad para concentrase? Genera máximo 3 parrafos con recomendaciones.")
+print(run.content)
 
 # Print the response in the terminal
-asyncio.run(agent.aprint_response("Share a 2 sentence horror story"))
+# asyncio.run(agent.aprint_response("Share a 2 sentence horror story"))
