fc1c829c3165b1549a1ae56ef09673ff0b63b290
Jose <pepeisa.ortiz@gmail.com>
2025-10-11 18:46:22 -0500

    adaptado con ollama

--- cookbook/tools/hackernews_tools.py
+++ cookbook/tools/hackernews_tools.py
@@ -1,7 +1,11 @@
 from agno.agent import Agent
 from agno.tools.hackernews import HackerNewsTools
-
+from agno.models.ollama import Ollama
+# Por defecto deberia de funcionar con la api de open ai configurada en .env
+# Para mi caso de uso lo utilice con ollama y funciono
+# solo se agrego: model=Ollama(id="qwen3:8b")
 agent = Agent(
+    model=Ollama(id="qwen3:8b"),
     name="Hackernews Team",
     tools=[HackerNewsTools()],
     show_tool_calls=True,
